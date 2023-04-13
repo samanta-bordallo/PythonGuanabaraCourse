fe585b8f4a2bed9bd101c15cfd17b61953cf016d
samanta-bordallo <bordallo.samanta@gmail.com>
2023-04-13 23:57:07 +0100

    Implemented up to exercise #7.

--- main.py
+++ main.py
@@ -23,6 +23,18 @@ def new_word():
     print(f"Está em minusculas? {str.islower(word)}")
     print(f"Está capitalizada? {str.istitle(word)}")
 
+def suc_ant():
+    number = int(input('Diga um número '))
+    print(f'O antecessor de {number} é {number - 1} e o sucessor é {number + 1}')
+
+def dobro():
+    number2 = int(input('Diga um número '))
+    print(f'o dobro de {number2} é {number2 * 2}, o triplo é {number2 * 3} e a raiz quadrada é {number2 ** (1/2)}')
+
+def media_notas():
+    nota1 = int(input("Qual a primeira nota? "))
+    nota2 = int(input("Qual a sugunda nota? "))
+    print(f'A média da nota é {(nota1 + nota2) / 2}')
 
 # Menus
 def menu_world1():
@@ -32,6 +44,9 @@ def menu_world1():
         print("2. your name")
         print("3. Math...")
         print("4. One word")
+        print("5. Qual o antecessor")
+        print("6. dobro")
+        print("7. Media notas")
         print("0. quit")
 
         world1_input = input("\nEnter your choice: ")
@@ -47,6 +62,15 @@ def menu_world1():
 
         elif world1_input == "4":
             new_word()
+
+        elif world1_input == "5":
+            suc_ant()
+
+        elif world1_input == "6":
+            dobro()
+
+        elif world1_input == "7":
+            media_notas()
 
         elif world1_input == "0":
             print("Goodbye")
